af9b7a483d6b5ca9ec3b314a5e30dc77cb6237e1
Roger Teo <roger.myop@gmail.com>
2024-11-27 22:55:50 +0800

    added get route called 'greet'

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/greet', methods=['GET'])
+def greet():
+    name = request.args.get('name', 'User')
+    return f'Hello, {name}!'
 
 if __name__ == '__main__':
     app.run(debug=True)
